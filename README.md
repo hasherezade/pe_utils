7a05e746408dd99f62862b665044f9cea586849c
hasherezade <hasherezade@gmail.com>
2021-03-13 15:05:33 +0100

    [FEATURE][dll_load] Added a utility to load DLLs

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-# pe_check
+# PE utils
 [![Build status](https://ci.appveyor.com/api/projects/status/d59ouubyha1h6n9f?svg=true)](https://ci.appveyor.com/project/hasherezade/pe-check)
 
-A simple app checking the PE architecture (a helper for a PIN tracer)
+Set of small utilities, helpers for PIN Tools
+
++ **pe_check** - an app checking the PE architecture
++ **dll_load** - loads a DLL
---
+++ dll_load/main.cpp
@@ -0,0 +1,74 @@
+#include <Windows.h>
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+size_t split_list(const std::string &sline, const char delimiter, std::vector<std::string> &args)
+{
+	std::istringstream f(sline);
+	std::string s;
+	while (getline(f, s, delimiter)) {
+		args.push_back(s);
+	}
+	return args.size();
+}
+
+int wmain(int argc, wchar_t *argv[])
+{
+#ifdef _WIN64
+	bool is_on_64 = true;
+#else
+	bool is_on_64 = false;
+#endif
+	if (argc < 2) {
+		std::cout << "Loads a given DLL. May call exported functions if supplied.\n";
+		if (is_on_64) {
+			std::cout << "64-bit version\n" << std::endl;
+		}
+		else {
+			std::cout << "32-bit version\n" << std::endl;
+		}
+		std::cout << "args: <path to the PE>" << std::endl;
+		std::cout << "Args: <DLL> [*exports]\n";
+		std::cout << "\t * - optional]\n";
+		system("pause");
+		return 0;
+	}
+	wchar_t* pe_path = argv[1];
+	HMODULE lib =  LoadLibraryW(pe_path);
+	if (!lib) {
+		return 0;
+	}
+	if (argc < 3) {
+		// no more args to process
+		return 1;
+	}
+	std::wstring paramsl = argv[2];
+	std::string params(paramsl.begin(), paramsl.end());
+	std::vector<std::string> exports;
+	split_list(params, ';', exports);
+
+	std::vector<std::string>::iterator itr;
+	for (itr = exports.begin(); itr != exports.end(); itr++) {
+		std::string func_name = *itr;
+		FARPROC func = NULL;
+		if (func_name[0] == '#') {
+			int ordinal;
+			std::string ord_str = func_name.substr(1);
+			std::stringstream ss;
+			ss << std::dec << ord_str;
+			ss >> ordinal;
+			func = GetProcAddress(lib, MAKEINTRESOURCE(ordinal));
+		}
+		else {
+			func = GetProcAddress(lib, func_name.c_str());
+		}
+		
+		if (!func) continue;
+
+		std::cout << "Calling the export: " << func_name << "\n";
+		int(*exp_func)() = (int(*)())func;
+		func();
+	}
+	return 0;
+}

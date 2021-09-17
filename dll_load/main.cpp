2ef793aea682c943be4f79b648c1f8a27a1feae9
hasherezade <hasherezade@gmail.com>
2021-09-17 16:22:51 +0200

    [REFACT][dll_load] Refactored. Added pause at the end

--- dll_load/main.cpp
+++ dll_load/main.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 #include <sstream>
 
+#define PAUSE_AFTER
+
 size_t split_list(const std::string &sline, const char delimiter, std::vector<std::string> &args)
 {
 	std::istringstream f(sline);
@@ -11,6 +13,37 @@ size_t split_list(const std::string &sline, const char delimiter, std::vector<st
 		args.push_back(s);
 	}
 	return args.size();
+}
+
+bool run_dll_with_args(const wchar_t* pe_path, std::vector<std::string> &exports)
+{
+	HMODULE lib = LoadLibraryW(pe_path);
+	if (!lib) {
+		return false;
+	}
+	std::vector<std::string>::iterator itr;
+	for (itr = exports.begin(); itr != exports.end(); itr++) {
+		std::string func_name = *itr;
+		FARPROC func = NULL;
+		if (func_name[0] == '#') {
+			int ordinal;
+			std::string ord_str = func_name.substr(1);
+			std::stringstream ss;
+			ss << std::dec << ord_str;
+			ss >> ordinal;
+			func = GetProcAddress(lib, MAKEINTRESOURCE(ordinal));
+		}
+		else {
+			func = GetProcAddress(lib, func_name.c_str());
+		}
+
+		if (!func) continue;
+
+		std::cout << "Calling the export: " << func_name << "\n";
+		int(*exp_func)() = (int(*)())func;
+		func();
+	}
+	return true;
 }
 
 int wmain(int argc, wchar_t *argv[])
@@ -38,41 +71,21 @@ int wmain(int argc, wchar_t *argv[])
 		return 0;
 	}
 	wchar_t* pe_path = argv[1];
-	HMODULE lib =  LoadLibraryW(pe_path);
-	if (!lib) {
-		return is_dll_executed;
-	}
-	is_dll_executed = 1;
-	if (argc < 3) {
-		// no more args to process
-		return is_dll_executed;
-	}
-	std::wstring paramsl = argv[2];
-	std::string params(paramsl.begin(), paramsl.end());
+
 	std::vector<std::string> exports;
-	split_list(params, ';', exports);
+	if (argc >= 3) {
+		// load exports:
+		std::wstring paramsl = argv[2];
+		std::string params(paramsl.begin(), paramsl.end());
+		split_list(params, ';', exports);
+	}
 
-	std::vector<std::string>::iterator itr;
-	for (itr = exports.begin(); itr != exports.end(); itr++) {
-		std::string func_name = *itr;
-		FARPROC func = NULL;
-		if (func_name[0] == '#') {
-			int ordinal;
-			std::string ord_str = func_name.substr(1);
-			std::stringstream ss;
-			ss << std::dec << ord_str;
-			ss >> ordinal;
-			func = GetProcAddress(lib, MAKEINTRESOURCE(ordinal));
-		}
-		else {
-			func = GetProcAddress(lib, func_name.c_str());
-		}
-		
-		if (!func) continue;
-
-		std::cout << "Calling the export: " << func_name << "\n";
-		int(*exp_func)() = (int(*)())func;
-		func();
+	if (run_dll_with_args(pe_path, exports)) {
+		is_dll_executed = 1;
+		std::cout << "[+] The Dll was run! " << std::endl;
+#ifdef PAUSE_AFTER
+		system("pause");
+#endif
 	}
 	return is_dll_executed;
 }

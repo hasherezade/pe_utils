e59091a0a9d0c7d7cfb2d2cac001c05803413e1c
hasherezade <hasherezade@gmail.com>
2023-03-18 07:47:20 -0700

    [FEATURE][syscall_extract] Extract from both ntdll and win32u

--- syscall_extractor/main.cpp
+++ syscall_extractor/main.cpp
@@ -5,7 +5,7 @@
 #include <peconv.h> // include libPeConv header
 #include "util.h"
 
-size_t extract_syscalls(BYTE* pe_buf, size_t pe_size, std::stringstream& outs)
+size_t extract_syscalls(BYTE* pe_buf, size_t pe_size, std::stringstream& outs, size_t startID = 0)
 {
 	std::vector<std::string> names_list;
 	if (!peconv::get_exported_names(pe_buf, names_list)) {
@@ -23,7 +23,7 @@ size_t extract_syscalls(BYTE* pe_buf, size_t pe_size, std::stringstream& outs)
 			sys_functions[rva] = funcName;
 		}
 	}
-	size_t id = 0;
+	size_t id = startID;
 	for (auto itr = sys_functions.begin(); itr != sys_functions.end(); ++itr) {
 		std::string funcName = itr->second;
 		outs << std::hex << "0x" << id++ << "," << funcName << "\n";
@@ -31,33 +31,63 @@ size_t extract_syscalls(BYTE* pe_buf, size_t pe_size, std::stringstream& outs)
 	return id;
 }
 
+size_t extract_from_dll(IN const std::string &path, size_t startSyscallID, OUT std::stringstream &outs)
+{
+	size_t bufsize = 0;
+	BYTE* buffer = peconv::load_pe_module(path.c_str(), bufsize, false, false);
+
+	if (!buffer) {
+		std::cerr << "Failed to load the PE: " << path << "\n";
+		return 0;
+	}
+
+	size_t extracted_count = extract_syscalls(buffer, bufsize, outs, startSyscallID);
+	peconv::free_pe_buffer(buffer);
+
+	if (!extracted_count) {
+		std::cerr << "No syscalls extracted from: " << path << "\n";
+	}
+	return extracted_count;
+}
 
 int main(int argc, char *argv[])
 {
+	LPCSTR pe_path = NULL;
 	if (argc < 2) {
-		std::cout << "Extract syscalls from system DLLs.\n"
-			<< "Arg: <DLL>"
+		std::cout << "Extract syscalls from system DLLs (ntdll.dll, win32u.dll)\n"
+			<< "Optional Arg: <DLL path>"
 			<< std::endl;
-		system("pause");
-		return 0;
 	}
-	LPCSTR pe_path = argv[1];
-	size_t bufsize = 0;
+	else {
+		pe_path = argv[1];
+	}
 
-	PVOID old_val;
+	PVOID old_val = NULL;
 	util::wow64_disable_fs_redirection(&old_val);
-	BYTE *buffer = peconv::load_pe_module(pe_path, bufsize, false, false);
+
+	std::stringstream outs;
+	size_t extracted_count = 0;
+
+	if (pe_path) {
+		extracted_count += extract_from_dll(pe_path, 0, outs);
+	}
+	else {
+		char ntdll_path[MAX_PATH] = { 0 };
+		ExpandEnvironmentStringsA("%SystemRoot%\\system32\\ntdll.dll", ntdll_path, MAX_PATH);
+		extracted_count += extract_from_dll(ntdll_path, 0, outs);
+
+		char win32u_path[MAX_PATH] = { 0 };
+		ExpandEnvironmentStringsA("%SystemRoot%\\system32\\win32u.dll", win32u_path, MAX_PATH);
+		extracted_count += extract_from_dll(win32u_path, 0x1000, outs);
+	}
+
 	util::wow64_revert_fs_redirection(&old_val);
 
-	if (!buffer) {
-		std::cerr << "Failed to load the PE!\n";
+	if (!extracted_count) {
+		std::cerr << "Failed to extract syscalls.\n";
 		return 0;
 	}
-	std::stringstream outs;
-	if (!extract_syscalls(buffer, bufsize, outs)) {
-		std::cerr << "No syscalls extracted!\n";
-		return 0;
-	}
+
 	std::string outFileName = "syscalls.txt";
 	std::ofstream myfile;
 	myfile.open(outFileName);
